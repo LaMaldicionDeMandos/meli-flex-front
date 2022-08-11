1f12ea42609b4024300b40921fc6c39d09a8f97b
marcelo <pasutmarcelo@gmail.com>
2022-08-11 10:36:05 -0300

    trabajo en mejorar los componentes del header de las ordenes

--- src/components/DeliveryOrder/ActiveDeliveryOrder.js
+++ src/components/DeliveryOrder/ActiveDeliveryOrder.js
@@ -17,8 +17,6 @@
 */
 import React, {useState} from "react";
 
-import SweetAlert from "react-bootstrap-sweetalert";
-
 import { map } from 'lodash';
 import moment from 'moment';
 import 'moment/locale/es';
@@ -26,96 +24,24 @@ import 'moment/locale/es';
 // reactstrap components
 import {
   Row,
-  Col, Button
+  Col
 } from "reactstrap";
 import OrderRowMin from "../OrderRow/OrderRowMin";
 import currencyFormatter from '../../utils/currency.formatter';
-import DeliveryOrderStatusIcon from "../DeliveryOrderStatusIcon/DeliveryOrderStatusIcon";
-import deliveryOrderService from "../../services/deliveryOrder.service";
-import paymentsService from "../../services/payments.service";
-
-const PENDING_STATUS = 'pending';
-const PAID_STATUS = 'paid';
-
-const DEFAULT_WAITING_MINUTES = 30;
-const ONE_SECOND = 1000;
-const INTERVAL_KEY = 'activeDeliveryOrderInterval';
-
-function TimeRemaining({time}) {
-  const duration = moment.duration(time, 'seconds');
-  return 'Expira ' + duration.locale('es').humanize(true);
-}
+import ActiveDeliveryOrderHeaderExtra from "./ActiveDeliveryOrderHeaderExtra";
 
 function ActiveDeliveryOrder({order}) {
   const [orderStatus, setOrderStatus] = useState(order.status);
-  const [showReActiveDeliveryDialog, setShowReActiveDeliveryDialog] = useState(false);
-  const [showRequestButton, setShowRequestButton] = useState(true);
-  const [waitingPayment, setWaitingPayment] = useState(false);
 
   const orderList = map(order.orders, (order) => <OrderRowMin key={order.id} order={order} />);
 
-  const reActivateDeliveryOrder = (minutes) => {
-    deliveryOrderService.reActivateDeliveryOrder(order._id, minutes)
-      .then(result => {
-          paymentsService.chackout(result.id, `#order${order._id}`);
-          setShowRequestButton(false);
-          //refreshHandler()
-        });
-  }
-
-  const getOrderStatus = () => {
-    deliveryOrderService.getDeliveryOrderStatus(order._id)
-      .then(result => {
-        console.log(`Order status ${result.status}`);
-        setOrderStatus(result.status);
-        if (result.status === PAID_STATUS) {
-          clearInterval(Number.parseInt(window.sessionStorage.getItem(INTERVAL_KEY)));
-        }
-        setWaitingPayment(false);
-      })
-  }
-
-  const onMercadopagoClick = () => {
-    setWaitingPayment(true);
-    const intervalId = setInterval(getOrderStatus, ONE_SECOND);
-    window.sessionStorage.setItem(INTERVAL_KEY, intervalId);
-  }
-
   return (
     <Row>
-      {showReActiveDeliveryDialog ? <SweetAlert
-        input
-        inputType="number"
-        required={true}
-        showCancel
-        defaultValue={DEFAULT_WAITING_MINUTES}
-        title="Tiempo de espera hasta que expire la orden de reparto"
-        confirmBtnText="Confirmar"
-        confirmBtnBsStyle="primary"
-        cancelBtnText="Ahora no"
-        cancelBtnBsStyle="danger"
-        onCancel={() => setShowReActiveDeliveryDialog(false)}
-        onConfirm={(minutes) => {
-          reActivateDeliveryOrder(minutes);
-          setShowReActiveDeliveryDialog(false);
-        }}
-      >Si no encontramos un repartidor antes de que expire el tiempo, te reenbolsaremos el dinero.</SweetAlert> : ''}
       <Col md="12">
         <h5 style={{marginTop: 5, marginBottom: 5, marginLeft:10, fontWeight: 'bold'}}>
           <Row>
             <Col md="3">
-              <DeliveryOrderStatusIcon status={orderStatus}>
-                {orderStatus === PAID_STATUS ? (<label className="title-row" style={{marginLeft: 6, fontWeight: 'normal'}}><TimeRemaining time={order.ttl}/></label>) : ''}
-              </DeliveryOrderStatusIcon>
-              {orderStatus === PENDING_STATUS
-                ? (
-                  <>{waitingPayment ? '' : <div id={`order${order._id}`} style={{paddingRight: 16}} onClick={onMercadopagoClick}></div>}
-                  {showRequestButton
-                    ? <Button className="btn-round btn-sm btn-primary" onClick={() => setShowReActiveDeliveryDialog(true)}>Volver a publicar</Button>
-                    : ''
-                  }</>
-                )
-                : ''}
+              <ActiveDeliveryOrderHeaderExtra order={order} status={orderStatus} changeStatusHandler={setOrderStatus}/>
             </Col>
             <Col md="7" className="text-center">
               <span>{order.name}</span>
---
+++ src/components/DeliveryOrder/ActiveDeliveryOrderHeaderExtra.js
@@ -0,0 +1,121 @@
+import DeliveryOrderStatusIcon from "../DeliveryOrderStatusIcon/DeliveryOrderStatusIcon";
+import SweetAlert from "react-bootstrap-sweetalert";
+import {Button, Row} from "reactstrap";
+import React, {useState} from "react";
+
+import moment from 'moment';
+import 'moment/locale/es';
+import deliveryOrderService from "../../services/deliveryOrder.service";
+import paymentsService from "../../services/payments.service";
+
+const PENDING_STATUS = 'pending';
+const PAID_STATUS = 'paid';
+
+const DEFAULT_WAITING_MINUTES = 30;
+const ONE_SECOND = 1000;
+const INTERVAL_KEY = 'activeDeliveryOrderInterval';
+
+function TimeRemaining({time}) {
+  const duration = moment.duration(time, 'seconds');
+  return time > 0 ? 'Expira ' + duration.locale('es').humanize(true) : '';
+}
+
+function ActiveDeliveryOrderHeaderExtra({order, status, changeStatusHandler = () => {}}) {
+  const [waitingPayment, setWaitingPayment] = useState(false);
+  const [showReActiveDeliveryDialog, setShowReActiveDeliveryDialog] = useState(false);
+  const [showRequestButton, setShowRequestButton] = useState(true);
+
+  const reActivateDeliveryOrder = (minutes) => {
+    deliveryOrderService.reActivateDeliveryOrder(order._id, minutes)
+      .then(result => {
+        paymentsService.chackout(result.id, `#order${order._id}`);
+        setShowRequestButton(false);
+      });
+  }
+
+  const getOrderStatus = () => {
+    deliveryOrderService.getDeliveryOrderStatus(order._id)
+      .then(result => {
+        console.log(`Order status ${result.status}`);
+        changeStatusHandler(result.status);
+        if (result.status === PAID_STATUS) {
+          clearInterval(Number.parseInt(window.sessionStorage.getItem(INTERVAL_KEY)));
+        }
+        setWaitingPayment(false);
+      })
+  }
+
+  const onMercadopagoClick = () => {
+    setWaitingPayment(true);
+    const intervalId = setInterval(getOrderStatus, ONE_SECOND);
+    window.sessionStorage.setItem(INTERVAL_KEY, intervalId);
+  }
+
+  const statusIconExtra = () => {
+    if (status === PENDING_STATUS) return pendingStatusIconExtra();
+    if (status === PAID_STATUS) return paidStatusIconExtra();
+    else return workingStatusIconExtra();
+  }
+
+  const pendingStatusIconExtra = () => {
+    return '';
+  }
+
+  const paidStatusIconExtra = () => {
+    return <label className="title-row" style={{marginLeft: 6, fontWeight: 'normal'}}><TimeRemaining time={order.ttl}/></label>
+  }
+
+  const workingStatusIconExtra = () => {
+    return '';
+  }
+
+  const headerExtra = () => {
+    if (status === PENDING_STATUS) return pendingHeaderExtra();
+    if (status === PAID_STATUS) return paidHeaderExtra();
+    else return workingHeaderExtra();
+  }
+
+  const pendingHeaderExtra = () => {
+    return (<>{waitingPayment
+      ? ''
+      : <div id={`order${order._id}`} style={{paddingRight: 16}} onClick={onMercadopagoClick}></div>}
+      {showRequestButton
+        ? <Button className="btn-round btn-sm btn-primary" onClick={() => setShowReActiveDeliveryDialog(true)}>Volver a publicar</Button>
+        : ''
+      }</>);
+  }
+
+  const paidHeaderExtra = () => {
+    return '';
+  }
+
+  const workingHeaderExtra = () => {
+    return '';
+  }
+
+  return (
+    <>
+      {showReActiveDeliveryDialog ? <SweetAlert
+        input
+        inputType="number"
+        required={true}
+        showCancel
+        defaultValue={DEFAULT_WAITING_MINUTES}
+        title="Tiempo de espera hasta que expire la orden de reparto"
+        confirmBtnText="Confirmar"
+        confirmBtnBsStyle="primary"
+        cancelBtnText="Ahora no"
+        cancelBtnBsStyle="danger"
+        onCancel={() => setShowReActiveDeliveryDialog(false)}
+        onConfirm={(minutes) => {
+          reActivateDeliveryOrder(minutes);
+          setShowReActiveDeliveryDialog(false);
+        }}
+      >Si no encontramos un repartidor antes de que expire el tiempo, te reenbolsaremos el dinero.</SweetAlert> : ''}
+      <DeliveryOrderStatusIcon status={status}>
+        {statusIconExtra()}
+      </DeliveryOrderStatusIcon>
+      {headerExtra()}
+    </>);
+}
+export default ActiveDeliveryOrderHeaderExtra;
